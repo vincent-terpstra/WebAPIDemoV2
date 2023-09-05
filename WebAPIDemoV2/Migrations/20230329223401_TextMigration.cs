b2c601d6622b5d0ba908b3684bde1d0d792ff5c3
Vincent Terpstra <vdterpst@gmail.com>
2023-09-05 18:37:27 -0400

    Adding test project and initial EF migrations

---
+++ WebAPIDemoV2/Migrations/20230329223401_TextMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIDemoV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class TextMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Texts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Texts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Texts");
+        }
+    }
+}
